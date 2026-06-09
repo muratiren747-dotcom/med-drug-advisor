fc156eea26e106e1b74e2b37a11e152fd6b12098
Batuhan Balkaya <balkayabatuhan41@gmail.com>
2026-06-09 17:20:31 +0300

    Update models.py

--- backend/core/models.py
+++ backend/core/models.py
@@ -22,34 +22,33 @@ class Drug:
                 "max_dose": self.max_dose,
                 "food_interactions": self.food_interactions,
                 "risk_factors": self.risk_factors}
-        
-    class Patient:
-        """
-        Patient class that stores the patient information (personal and medical profile of user) from the database_mgr.py
-        Patient object is built right after the successful login to the database
-        """
-        def __init__(self, username, age, sex, weight, medical_conditions = None, is_pregnant = False):
-            self.username = username
+class Patient:
+    """
+    Patient class that stores the patient information (personal and medical profile of user) from the database_mgr.py
+    Patient object is built right after the successful login to the database
+    """
+    def __init__(self, username, age, sex, weight, medical_conditions = None, is_pregnant = False):
+        self.username = username
+        self.age = age
+        self.sex = sex
+        self.weight = weight
+        self.medical_conditions = medical_conditions if medical_conditions is not None else []
+        self.is_pregnant = is_pregnant
+        self.history = []
+
+    def update_profile(self, age = None, sex = None, weight = None, medical_conditions = None, is_pregnant = None):
+        if age != None:
             self.age = age
+        if sex != None:
             self.sex = sex
+        if weight != None:
             self.weight = weight
-            self.medical_conditions = medical_conditions if medical_conditions is not None else []
+        if medical_conditions != None:
+            self.medical_conditions = medical_conditions
+        if is_pregnant != None:
             self.is_pregnant = is_pregnant
-            self.history = []
 
-        def update_profile(self, age = None, sex = None, weight = None, medical_conditions = None, is_pregnant = None):
-            if age != None:
-                self.age = age
-            if sex != None:
-                self.sex = sex
-            if weight != None:
-                self.weight = weight
-            if medical_conditions != None:
-                self.medical_conditions = medical_conditions
-            if is_pregnant != None:
-                self.is_pregnant = is_pregnant
-
-            """
-            Method that updates patient information when the user edits their profile.
-            Only fields that are provided (not None) will be updated.
-            """
+        """
+        Method that updates patient information when the user edits their profile.
+        Only fields that are provided (not None) will be updated.
+        """
